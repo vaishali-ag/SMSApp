e12c1514b2cd897268862e4f10aee0e5336c9214
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-28 11:59:38 +0530

    done

--- app/src/main/java/vaisahli/agrawal/smsapp/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/smsapp/MainActivity.java
@@ -1,5 +1,7 @@
 package vaisahli.agrawal.smsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +21,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        EditText phone,message;
+        message = findViewById(R.id.editText3);
+        phone = findViewById(R.id.editText2);
 
+        message = findViewById(R.id.editText3);
+        phone = findViewById(R.id.editText2);
         FloatingActionButton fab = findViewById(R.id.fab);
+        final EditText finalMessage = message;
+        final EditText finalPhone = phone;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String s1 = finalPhone.getText().toString().trim();
+                String s2 = finalMessage.getText().toString().trim();
+                if(s1.equals("")||s2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Enter all values", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String number = String.format("smsto:%s", s1);
+                    String  message=s2;
+                    Intent smsIntent= new Intent(Intent.ACTION_SENDTO);
+                    smsIntent.setData(Uri.parse(number));
+                    smsIntent.putExtra("sms_body", message);
+                  startActivity(smsIntent);
+
+
+                }
             }
         });
     }
