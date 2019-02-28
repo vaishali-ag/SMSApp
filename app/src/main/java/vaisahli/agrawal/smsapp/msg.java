27a218526d0edff15c9ef2e3c7a0b093c977a733
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-28 11:20:57 +0530

    add new activity msg

---
+++ app/src/main/java/vaisahli/agrawal/smsapp/msg.java
@@ -0,0 +1,13 @@
+package vaisahli.agrawal.smsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class msg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msg);
+    }
+}
